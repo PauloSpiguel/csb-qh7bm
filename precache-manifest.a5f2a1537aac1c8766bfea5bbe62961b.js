eeb609b1e776135b5b0108845775bcec3b78eaf8
Code Sandbox <hello@codesandbox.io>
2021-11-10 23:49:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a5f2a1537aac1c8766bfea5bbe62961b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e08b1fbcf993587010abb8f8f787940",
+    "url": "https://paulospiguel.github.io/csb-qh7bm/index.html"
+  },
+  {
+    "revision": "13b7cc9a71355935be50",
+    "url": "https://paulospiguel.github.io/csb-qh7bm/static/css/main.d8be9568.chunk.css"
+  },
+  {
+    "revision": "16331f4b3afc0a099b29",
+    "url": "https://paulospiguel.github.io/csb-qh7bm/static/js/2.8b6a866b.chunk.js"
+  },
+  {
+    "revision": "13b7cc9a71355935be50",
+    "url": "https://paulospiguel.github.io/csb-qh7bm/static/js/main.d4862c37.chunk.js"
+  },
+  {
+    "revision": "89dab6d2f52385021a4d",
+    "url": "https://paulospiguel.github.io/csb-qh7bm/static/js/runtime~main.518b80d4.js"
+  }
+]);
